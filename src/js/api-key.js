709a864f7a7b4c3117a2286b7df934e4e618ad32
prozhyvarov <121400043+prozhyvarov@users.noreply.github.com>
2023-06-01 20:00:18 -0600

    update partials, add folder js

---
+++ src/js/api-key.js
@@ -0,0 +1,7 @@
+const KEY = 'f1a48bce74b470ddc6475541cec139b4';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMG_BASE_URL = `https://image.tmdb.org/t/p`;
+const UPCOMING_URL = `${BASE_URL}/movie/upcoming`;
+const IMG_W400 = `/w400`;
+
+export { KEY, BASE_URL, IMG_BASE_URL, IMG_W400, UPCOMING_URL };
---
+++ src/js/home-upcoming.js
@@ -0,0 +1,15 @@
+const KEY = 'f1a48bce74b470ddc6475541cec139b4';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMG_BASE_URL = `https://image.tmdb.org/t/p`;
+const UPCOMING_URL = `${BASE_URL}/movie/upcoming`;
+const IMG_W400 = `/w400`;
+
+const upcomingContainer = document.querySelector('.upcoming__container');
+
+console.log('Hello');
+
+
+
+
+
+
