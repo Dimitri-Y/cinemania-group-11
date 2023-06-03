b9b22e0d83ec2b109f8893a98026c5b2b490726e
Kateryna1706 <silckatya@gmail.com>
2023-06-03 17:53:41 +0200

    in progress

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -19,15 +19,18 @@ const refs = {
   pagination: document.querySelector('.pagination'),
 };
 
+refs.pagination.addEventListener('click', onClick);
+
 const options = {
   totalItems: 500,
   itemsPerPage: 10,
-  visiblePages: 5,
+  visiblePages: 4,
   template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}p</a>',
-    currentPage: '<a href="" class="pagination-list__link">01</a>',
+    page:
+    `<a href="#" class="tui-page-btn pagination-list__link">01</a>`,
+    currentPage: `<a href="#" class="pagination-list__link selected">${page}</a>`,
     moveButton:
-      `<a href="" class="pagination__back arrow"></a>` +
+      `<a href="" class="pagination__back arrow tui-page-btn tui-{{type}} custom-class-{{type}}"></a>` +
       `<a href="" class="pagination__forward arrow"></a>`,
     disabledMoveButton:
       '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
@@ -38,3 +41,13 @@ const options = {
 };
 
 const pagination = new Pagination('pagination', options);
+
+function onClick(event) {
+  event.preventDefault();
+  refs.paginationListLinks.forEach(item => {
+    if (event.target === item) {
+      page = event.target.textContent;
+      console.log(page);
+    }
+  });
+}
