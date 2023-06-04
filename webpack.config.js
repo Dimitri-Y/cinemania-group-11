36e93144f6cfca1a03e24ac1e63620b30e9f722a
Dimitri-Y <dimoon.bond@gmail.com>
2023-06-05 00:57:52 +0300

    import webpack,image-webpack-loader

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  module: [
+    rules[
+      {
+        test: /\.(jpg|png|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './',
+              useRelativePath: true,
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 70,
+              },
+            },
+          },
+        ],
+      }
+    ],
+  ],
+};
