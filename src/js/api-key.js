5cffb1eab6b8b8e92b77d6aed82337b3d42ce866
TarasMysiura <tarasmysiura@gmail.com>
2023-06-05 19:30:54 +0300

    add logic

--- src/js/api-key.js
+++ src/js/api-key.js
@@ -3,6 +3,8 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_BASE_URL = `https://image.tmdb.org/t/p`;
 const UPCOMING_URL = `${BASE_URL}/movie/upcoming`;
 const TREND_URL = `${BASE_URL}/trending/movie/week`;
+const VIDEO_URL = `${BASE_URL}/movie`;
+
 const IMG_W400 = `/w400`;
 
-export { KEY, BASE_URL, IMG_BASE_URL, IMG_W400, UPCOMING_URL, TREND_URL };
+export { KEY, BASE_URL, IMG_BASE_URL, IMG_W400, UPCOMING_URL, TREND_URL, VIDEO_URL };
