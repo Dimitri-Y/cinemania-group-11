8326e6ca77757097719086370c04a7ca4dd7cbce
YaremaNatalia <yaremka.n@gmail.com>
2023-06-06 15:58:36 +0300

    add star_rating_logic

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -5,13 +5,48 @@ let page = 1;
 
 // ======ВИКЛИК ФУНКЦІЇ РЕНДЕРУ КАРТОК=======
 
-export const movieListContainer = document.querySelector('.catalog__gallery');
-fetchMovieTrend(page)
-  .then(data => {
-    renderCards(data, movieListContainer);
-  })
-  .catch(error => {
-    console.error('Error rendering movie cards:', error);
-  });
+// export const movieListContainer = document.querySelector('.catalog__gallery');
+// fetchMovieTrend(page)
+//   .then(data => {
+//     renderCards(data, movieListContainer);
+//   })
+//   .catch(error => {
+//     console.error('Error rendering movie cards:', error);
+//   });
+
+
+  export const movieListContainer = document.querySelector('.catalog__gallery');
+  fetchMovieTrend(page)
+    .then(data => {
+      initRatings(data);
+      renderCards(data, movieListContainer);
+    })
+    .catch(error => {
+      console.error('Error rendering movie cards:', error);
+    });
+
+  console.log(page);
+
+  const ratings = document.querySelectorAll('.rating');
+  if (ratings.length > 0) {
+    initRatings();
+  }
+
+  function initRatings(data) {
+    for (let index = 0; index < ratings.length; index++) {
+      const rating = ratings[index];
+      const voteAverage = data[index].vote_average;
+      console.log(voteAverage);
+      initRating(rating, voteAverage);
+    }
+  }
+
+  function initRating(rating, voteAverage) {
+    const ratingActive = rating.querySelector('.rating__active');
+    const ratingActiveWidth = voteAverage / 0.01;
+    ratingActive.style.width = `${ratingActiveWidth}%`;
+  }
 
 console.log(page);
+
+
