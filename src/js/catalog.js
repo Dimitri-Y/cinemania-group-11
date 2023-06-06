82b60a54b553fb88a1228bb95dc7c5a6d183364b
YaremaNatalia <yaremka.n@gmail.com>
2023-06-06 16:36:40 +0300

    fixes

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,19 +1,10 @@
 import { fetchMovieTrend } from './api';
 import { renderCards } from './movie_card';
+import {initRatings} from "./star_rating"
 
 let page = 1;
 
 // ======ВИКЛИК ФУНКЦІЇ РЕНДЕРУ КАРТОК=======
-
-// export const movieListContainer = document.querySelector('.catalog__gallery');
-// fetchMovieTrend(page)
-//   .then(data => {
-//     renderCards(data, movieListContainer);
-//   })
-//   .catch(error => {
-//     console.error('Error rendering movie cards:', error);
-//   });
-
 
   export const movieListContainer = document.querySelector('.catalog__gallery');
   fetchMovieTrend(page)
@@ -25,27 +16,9 @@ let page = 1;
       console.error('Error rendering movie cards:', error);
     });
 
-  console.log(page);
 
-  const ratings = document.querySelectorAll('.rating');
-  if (ratings.length > 0) {
-    initRatings();
-  }
 
-  function initRatings(data) {
-    for (let index = 0; index < ratings.length; index++) {
-      const rating = ratings[index];
-      const voteAverage = data[index].vote_average;
-      console.log(voteAverage);
-      initRating(rating, voteAverage);
-    }
-  }
-
-  function initRating(rating, voteAverage) {
-    const ratingActive = rating.querySelector('.rating__active');
-    const ratingActiveWidth = voteAverage / 0.01;
-    ratingActive.style.width = `${ratingActiveWidth}%`;
-  }
+  
 
 console.log(page);
 
--- src/js/star_rating.js
+++ src/js/star_rating.js
@@ -1,20 +1,27 @@
-// const ratings = document.querySelectorAll('.rating');
-// if (ratings.length > 0) {
-//   initRatings();
-// }
+const ratings = document.querySelectorAll('.rating');
+if (ratings.length > 0) {
+  initRatings();
+}
+;
+// присвоєння значення рейтингу
+function initRatings(data) {
+      console.log(ratings)
+    // console.log(data.results[0].vote_average);
+  
 
-// function initRatings() {
-//   for (let index = 0; index < ratings.length; index++) {
-//     const rating = ratings[index];
-//     initRating(rating);
-//   }
-//   // присвоїти індексу значення рейтингу
-//   function initRating(rating) {
-//     const ratingActive = rating.querySelector('.rating__active');
-//     const voteAverage = parseFloat(rating.dataset.voteAverage);
-//     const ratingActiveWidth = voteAverage / 0.01;
-//     ratingActive.style.width = `${ratingActiveWidth}%`;
-//   }
-// }
+  for (let index = 0; index < ratings.length; index++) {
+    const rating = ratings[index];
+    const voteAverage = data.results.vote_average;
 
-// export { initRatings };
+    initRating(rating, voteAverage);
+  }
+}
+
+function initRating(rating, voteAverage) {
+  console.log('hello');
+  const ratingActive = rating.querySelector('.rating__active');
+  const ratingActiveWidth = voteAverage / 0.01;
+  ratingActive.style.width = `${ratingActiveWidth}%`;
+}
+
+export { initRatings };
