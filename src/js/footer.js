88aa597d9fb3558953c41c83ca402a85d494ffd8
stilistica <119519958+stilistica@users.noreply.github.com>
2023-06-07 13:50:19 +0300

    update js

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,29 +1,28 @@
-(() => {
-  const refs = {
-    openModalBtnFooter: document.querySelector('[data-modal-open-footer]'),
-    closeModalBtnFooter: document.querySelector('[data-modal-close-footer]'),
-    modalFooter: document.querySelector('[data-modal-footer]'),
-    goBackModalBtmFooter: document.querySelector('[data-modal-go-back-footer]'),
-  };
+const refs = {
+  openModalBtnFooter: document.querySelector('[data-modal-open-footer]'),
+  closeModalBtnFooter: document.querySelector('[data-modal-close-footer]'),
+  modalFooter: document.querySelector('[data-modal-footer]'),
+  goBackModalBtnFooter: document.querySelector('[data-modal-go-back-footer]'),
+};
 
-  refs.openModalBtnFooter.addEventListener('click', openModal);
-  refs.closeModalBtnFooter.addEventListener('click', closeModal);
-  refs.goBackModalBtmFooter.addEventListener('click', closeModal);
-  window.addEventListener('keydown', onEscKeyPress);
+refs.openModalBtnFooter.addEventListener('click', openModalFooter);
+refs.closeModalBtnFooter.addEventListener('click', closeModalFooter);
+refs.goBackModalBtnFooter.addEventListener('click', closeModalFooter);
+window.addEventListener('keydown', onEscKeyPressFooter);
 
-  function openModal() {
-    refs.modalFooter.classList.remove('footer-contain__is-hidden');
-    document.body.style.overflow = 'hidden'; 
+function openModalFooter(event) {
+  event.preventDefault();
+  refs.modalFooter.classList.remove('footer-contain__is-hidden');
+  document.body.style.overflow = 'hidden';
+}
+
+function closeModalFooter() {
+  refs.modalFooter.classList.add('footer-contain__is-hidden');
+  document.body.style.overflow = '';
+}
+
+function onEscKeyPressFooter(event) {
+  if (event.code === 'Escape') {
+    closeModalFooter();
   }
-
-  function closeModal() {
-    refs.modalFooter.classList.add('footer-contain__is-hidden');
-    document.body.style.overflow = ''; 
-  }
-
-  function onEscKeyPress(event) {
-    if (event.code === 'Escape') {
-      closeModal();
-    }
-  }
-})();
+}
