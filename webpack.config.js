1472562327d8e878500e15ccbed7860fc1669f40
Dimitri-Y <dimoon.bond@gmail.com>
2023-06-07 22:07:53 +0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,34 @@
-// import { resolve } from 'path';
-// import { webpack } from 'webpack';
-
-// export const mode = 'production';
-// export const entry = './src/index.js';
-// export const output = {
-//   path: resolve(__dirname, 'dist'),
-//   filename: 'main.js',
-// };
-export const module = [
-  rules[
-    {
-      test: /\.(jpg|png|svg|gif)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'image',
-            useRelativePath: true,
-          },
-        },
-        {
-          loader: 'image-webpack-loader',
-          options: {
-            mozjpeg: {
-              progressive: true,
-              quality: 70,
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: [
+    rules[
+      {
+        test: /\.(jpg|png|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './',
+              useRelativePath: true,
             },
           },
-        },
-      ],
-    }
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 70,
+              },
+            },
+          },
+        ],
+      }
+    ],
   ],
-];
+};
