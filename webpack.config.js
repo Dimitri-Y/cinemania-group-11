2e20e5ba097d206dc9b6fddd5d56a6d37d93127b
Dimitri-Y <dimoon.bond@gmail.com>
2023-06-07 22:21:00 +0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,69 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js',
   },
-  module: [
-    rules[
+  module: {
+    rules: [
       {
-        test: /\.(jpg|png|svg|gif)$/,
-        use: [
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+        loaders: [
           {
             loader: 'file-loader',
             options: {
-              name: '[name].[ext]',
-              outputPath: './',
-              useRelativePath: true,
+              name: 'images/[name].[ext]',
             },
           },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 70,
-              },
-            },
-          },
+          'img-loader',
         ],
-      }
+      },
     ],
-  ],
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+    },
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+  },
+  performance: {
+    hints: false,
+  },
+  devtool: '#eval-source-map',
 };
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      beautify: false,
+      comments: false,
+      compress: {
+        sequences: true,
+        booleans: true,
+        loops: true,
+        unused: true,
+        warnings: false,
+        drop_console: true,
+        unsafe: true,
+      },
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+    }),
+  ]);
+}
