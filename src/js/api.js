747ca3f87beb6cd5eefc848c795991d67df5e488
YaremaNatalia <yaremka.n@gmail.com>
2023-06-10 01:53:05 +0300

    add scroll-color sprite-SVG

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
-import { KEY, TREND_URL } from './api-key';
+import {
+  KEY,
+  BASE_URL,
+  IMG_BASE_URL,
+  IMG_W400,
+  UPCOMING_URL,
+  TREND_DF_URL,
+  TREND_URL,
+  IMAGE_URL_W500,
+  VIDEO_URL,
+  SEARCH_URL,
+} from './api-key';
 
-// запит на трендові фільми тижня
+// === запит на трендові фільми тижня===
 async function fetchMovieTrend(page) {
   const PAGE = `&page=${page}`;
   return await axios
